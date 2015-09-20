6032534338c6ff1e9c29fa26edb553e8661ae1fe
Donggyu Kim <dgkim@eecs.berkeley.edu>
2015-09-19 22:35:57 -0700

    new RiscSRAM implementation

--- src/main/scala/RiscSRAM.scala
+++ src/main/scala/RiscSRAM.scala
@@ -7,89 +7,87 @@ class RiscSRAM extends Module {
   val io = new Bundle {
     val isWr   = Bool(INPUT)
     val wrAddr = UInt(INPUT, 8)
-    val wrData = Bits(INPUT, 32)
+    val wrData = UInt(INPUT, 32)
     val boot   = Bool(INPUT)
     val valid  = Bool(OUTPUT)
-    val out    = Bits(OUTPUT, 32)
+    val out    = UInt(OUTPUT, 32)
   }
-  val file = Mem(Bits(width = 32), 128, seqRead = true)
-  val code = Mem(Bits(width = 32), 256, seqRead = true)
+  val fileMem = SeqMem(UInt(width = 32), 128)
+  val codeMem = SeqMem(UInt(width = 32), 256)
 
-  val add_op :: imm_op :: Nil = Enum(Bits(), 2)
+  val idle :: decode :: ra_read :: rb_read :: rc_write :: Nil = Enum(UInt(), 5)
+  val code_read :: code_write :: Nil = Enum(UInt(), 2)
+  val fileState = RegInit(idle)
+  val codeState = RegInit(code_write)
 
-  val pc       = Reg(UInt(width=8))
-  val codeData = Reg(Bits(width=32))
-  val fileAddr = Reg(UInt(width=8))
-  val raData   = Reg(Bits(width=32))
-  val rbData   = Reg(Bits(width=32))
+  val add_op :: imm_op :: Nil = Enum(UInt(), 2)
+  val pc       = RegInit(UInt(0, 8))
+  val raData   = Reg(UInt(width=32))
+  val rbData   = Reg(UInt(width=32))
 
-  val inst = code(pc)
+  val code_wen = codeState === code_write && io.isWr
+  val code = codeMem.read(pc, !code_wen)
+  when(code_wen) {
+    codeMem.write(io.wrAddr, io.wrData)
+  }
+
+  val inst = Reg(UInt(width=32))
   val op   = inst(31,24)
   val rci  = inst(23,16)
   val rai  = inst(15, 8)
   val rbi  = inst( 7, 0)
+  val ra   = Mux(rai === UInt(0), UInt(0), raData)
+  val rb   = Mux(rbi === UInt(0), UInt(0), rbData)
 
   debug(op)
   debug(rci)
   debug(rai)
   debug(rbi)
 
-  val ra = Mux(rai === Bits(0), Bits(0), raData)
-  val rb = Mux(rbi === Bits(0), Bits(0), rbData)
-  val rc = Bits(width = 32)
+  io.out   := Mux(op === add_op, ra + rb, Cat(rai, rbi))
+  io.valid := fileState === rc_write && rci === UInt(255)
 
-  val file_init :: ra_read :: rb_read :: rc_write :: Nil = Enum(Bits(), 4)
-  val code_read :: code_write :: Nil = Enum(Bits(), 2)
-  val fileState = Reg(init=file_init)
-  val codeState = Reg(init=code_write)
-
-  io.valid := Bool(false)
-  io.out   := Bits(0)
-  rc       := Bits(0)
+  val file_wen = fileState === rc_write && rci != UInt(255)
+  val file_addr = Mux(fileState === decode, rai, rbi)
+  val file = fileMem.read(file_addr, !file_wen)
+  when(file_wen) {
+    fileMem.write(rci, io.out)
+  }
 
   switch(codeState) {
     is(code_write) {
-      when(io.isWr) {
-        code(io.wrAddr) := io.wrData
-      }. elsewhen(io.boot) {
-        pc        := UInt(0)
+      when(io.boot) {
+        inst := code
         codeState := code_read
-        fileState := file_init
       }
     }
     is(code_read) {
-      switch(op) {
-        is(add_op) { rc := ra + rb }
-        is(imm_op) { rc := rai << UInt(8) | rbi }
+      // execute
+    }
+  }
+  
+  switch(fileState) {
+    is(idle) {
+      when(io.boot) {
+        fileState := decode
       }
-      io.out := rc
-      switch(fileState) {
-        is(file_init) {
-          fileAddr := rai
-          when(op === add_op) {
-            fileState := ra_read
-          }.otherwise {
-            fileState := rc_write
-          }
-        }
-        is(ra_read) {
-          raData    := file(fileAddr)
-          fileAddr  := rbi
-          fileState := rb_read
-        }
-        is(rb_read) {
-          rbData    := file(fileAddr)
-          fileState := rc_write 
-        }
-        is(rc_write) {
-          when(rci === UInt(255)) {
-            io.valid  := Bool(true) 
-          }.otherwise {
-            file(rci) := rc
-          }
-          pc := pc + UInt(1)
-          fileState := file_init
-        }
+    }
+    is(decode) {
+      fileState := Mux(op === add_op, ra_read, rc_write)
+    }
+    is(ra_read) {
+      raData    := file
+      fileState := rb_read
+    }
+    is(rb_read) {
+      rbData    := file
+      fileState := rc_write
+    }
+    is(rc_write) {
+      when(!io.valid) {
+        fileState := decode
+        pc := pc + UInt(1)
+        inst := code
       }
     }
   }
@@ -176,7 +174,7 @@ class RiscSRAMTests(c: RiscSRAM) extends Tester(c) {
     I(c.add_op,   2, 2, 1), // r1 <- r1 + r1
     I(c.add_op,   2, 2, 1), // r1 <- r1 + r1
     I(c.add_op, 255, 1, 0)) // rh <- r1
-  wr(UInt(0), Bits(0)) // skip reset
+  wr(UInt(0), UInt(0)) // skip reset
   for (addr <- 0 until app.length) 
     wr(UInt(addr), app(addr))
   boot()
@@ -271,7 +269,7 @@ class RiscSRAMWrapperTests(c: SimWrapper[RiscSRAM]) extends SimWrapperTester(c) 
     I(c.target.add_op,   2, 2, 1), // r1 <- r1 + r1
     */
     I(c.target.add_op, 255, 1, 0)) // rh <- r1
-  wr(UInt(0), Bits(0)) // skip reset
+  wr(UInt(0), UInt(0)) // skip reset
   for (addr <- 0 until app.length) 
     wr(UInt(addr), app(addr))
   boot()
@@ -366,7 +364,7 @@ class RiscSRAMAXI4WrapperTests(c: SimAXI4Wrapper[SimWrapper[RiscSRAM]]) extends 
     I(c.sim.target.add_op,   2, 2, 1), // r1 <- r1 + r1
     */
     I(c.sim.target.add_op, 255, 1, 0)) // rh <- r1
-  wr(UInt(0), Bits(0)) // skip reset
+  wr(UInt(0), UInt(0)) // skip reset
   for (addr <- 0 until app.length) 
     wr(UInt(addr), app(addr))
   boot()

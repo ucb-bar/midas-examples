d59a85bc126b8fb588ddaddc8d6279fd8e63537f
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-09-30 16:28:42 -0700

    revert junctions

--- project/build.scala
+++ project/build.scala
@@ -6,14 +6,15 @@ object StroberBuild extends Build {
     scalaVersion := "2.11.7",
     scalacOptions ++= Seq("-deprecation","-unchecked")
   )
-  lazy val chisel   = project in file("riscv-mini/chisel")
-  lazy val firrtl   = project in file("riscv-mini/firrtl")
-  lazy val interp   = project in file("riscv-mini/interp") dependsOn firrtl
-  lazy val testers  = project in file("riscv-mini/testers") dependsOn (chisel, interp)
-  lazy val cde      = project in file("riscv-mini/cde") dependsOn chisel
-  lazy val mini     = project in file("riscv-mini") dependsOn (testers, cde)
-  lazy val tutorial = project dependsOn testers
-  lazy val strober  = project dependsOn mini
-  lazy val root     = project in file(".") settings (settings:_*) dependsOn (
+  lazy val chisel    = project in file("riscv-mini/chisel")
+  lazy val firrtl    = project in file("riscv-mini/firrtl")
+  lazy val interp    = project in file("riscv-mini/interp") dependsOn firrtl
+  lazy val testers   = project in file("riscv-mini/testers") dependsOn (chisel, interp)
+  lazy val cde       = project in file("riscv-mini/cde") dependsOn chisel
+  lazy val junctions = project in file("riscv-mini/junctions") dependsOn cde
+  lazy val mini      = project in file("riscv-mini") dependsOn (testers, junctions)
+  lazy val tutorial  = project dependsOn testers
+  lazy val strober   = project dependsOn mini
+  lazy val root      = project in file(".") settings (settings:_*) dependsOn (
     tutorial, strober, mini % "test->test")
 }

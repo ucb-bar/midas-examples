338db1d712d97bba1d6b11441f5a027870036623
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-10-17 10:57:52 -0700

    add replays to integration tests

--- src/test/scala/MiniSuite.scala
+++ src/test/scala/MiniSuite.scala
@@ -5,31 +5,45 @@ import scala.concurrent.{Future, Await, ExecutionContext}
 import scala.reflect.ClassTag
 import java.io.File
 
-abstract class MiniTestSuite(backend: String, latency: Int = 8, N: Int = 10) extends TestSuiteCommon {
+abstract class MiniTestSuite(latency: Int = 8, N: Int = 10) extends TestSuiteCommon {
   import scala.concurrent.duration._
   import ExecutionContext.Implicits.global
 
   val tp = cde.Parameters.root((new mini.MiniConfig).toInstance)
-  val target = compile(new mini.Tile(tp), backend)
-  val vcd = if (backend == "verilator") "vcd" else "vpd"
 
-  def runTests(testType: mini.TestType) {
-    behavior of testType.toString
+  def runTests(backend: String, testType: mini.TestType, debug: Boolean = false) {
+    val target = compile(new mini.Tile(tp), backend, debug)
+    val vcd = if (backend == "verilator") "vcd" else "vpd"
+    behavior of s"${testType.toString} in $backend"
     val results = testType.tests.zipWithIndex sliding (N, N) map { subtests =>
       val subresults = subtests map { case (t, i) =>
+        val sample = new File(resDir, s"$t-$backend.sample")
         val loadmem = Some(new File("hex", s"$t.hex"))
         val logFile = Some(new File("results", s"$t-$backend.log"))
         val waveform = Some(new File("results", s"$t-$backend.$vcd"))
-        Future(t -> run(target, backend, loadmem=loadmem, logFile=logFile, waveform=waveform))
+        val args = Seq(s"+latency=$latency", s"+sample=${sample.getAbsolutePath}")
+        Future(t -> run(target, backend, debug, loadmem, logFile, waveform, args))
       }
       Await.result(Future.sequence(subresults), Duration.Inf)
     }
     results.flatten foreach { case (name, exitcode) =>
       it should s"pass $name" in { assert(exitcode == 0) } }
+    if (p(strober.EnableSnapshot)) {
+      val replays = testType.tests.zipWithIndex sliding (N, N) map { subtests =>
+        val subreplays = subtests map { case (t, i) =>
+          val sample = new File(resDir, s"$t-$backend.sample")
+          Future(t -> replay(target, "vcs", Some(sample)))
+        }
+        Await.result(Future.sequence(subreplays), Duration.Inf)
+      }
+      replays.flatten foreach { case (name, exitcode) =>
+        it should s"replay $name in vcs" in { assert(exitcode == 0) } }
+    }
   }
-  runTests(mini.ISATests)
-  runTests(mini.BmarkTests)
+  compileReplay(new mini.Tile(tp), "vcs")
+  runTests("verilator", mini.ISATests)
+  runTests("verilator", mini.BmarkTests)
+  runTests("vcs", mini.ISATests)
+  runTests("vcs", mini.BmarkTests)
 }
-
-class MiniCppTests extends MiniTestSuite("verilator")
-class MiniVCSTests extends MiniTestSuite("vcs")
+class MiniTests extends MiniTestSuite()
--- src/test/scala/PointerChaserSuite.scala
+++ src/test/scala/PointerChaserSuite.scala
@@ -6,7 +6,9 @@ import scala.reflect.ClassTag
 import scala.sys.process.stringSeqToProcess
 import java.io.File
 
-abstract class PointerChaserTestSuite(backend: String, N: Int = 10) extends TestSuiteCommon {
+abstract class PointerChaserTestSuite(
+    seed: Long = System.currentTimeMillis,
+    N: Int = 5) extends TestSuiteCommon {
   import scala.concurrent.duration._
   import ExecutionContext.Implicits.global
 
@@ -15,26 +17,36 @@ abstract class PointerChaserTestSuite(backend: String, N: Int = 10) extends Test
   if (!loadmem.exists) assert(Seq(script.toString, "--output_file", loadmem.toString).! == 0)
 
   val tp = cde.Parameters.root((new PointerChaserConfig).toInstance)
-  val target = compile(new PointerChaser()(tp), backend)
-  val vcd = if (backend == "vcs") "vpd" else "vcd"
 
-  val results = (1 to 5) map (math.pow(2, _).toInt) map { latency =>
-    val logFile = Some(new File("results", s"PointerChaser-$backend-$latency.log"))
-    val waveform = Some(new File("results", s"PointerChaser-${latency}.${vcd}"))
-    val args = Seq(s"+latency=$latency")
-    Future(latency -> run(
-      target,
-      backend,
-      loadmem=Some(new File("init.hex")),
-      logFile=logFile,
-      waveform=waveform,
-      args=args)
-    )
+  def runTests(backend: String, debug: Boolean = false) {
+    val target = compile(new PointerChaser(seed)(tp), backend, debug)
+    val vcd = if (backend == "vcs") "vpd" else "vcd"
+    behavior of s"$target in $backend"
+    val results = (1 to N) map (math.pow(2, _).toInt) map { latency =>
+      val sample = new File(resDir, s"$target-$latency-$backend.sample")
+      val logFile = Some(new File("results", s"$target-$backend-$latency.log"))
+      val waveform = Some(new File("results", s"$target-${latency}.${vcd}"))
+      val args = Seq(s"+latency=$latency", s"+sample=${sample.getAbsolutePath}")
+      Future(latency ->
+        run(target, backend, debug, Some(new File("init.hex")), logFile, waveform, args)
+      )
+    }
+    Await.result(Future.sequence(results), Duration.Inf) foreach { case (latency, exitcode) =>
+      it should s"pass latency: $latency" in { assert(exitcode == 0) }
+    }
+    if (p(strober.EnableSnapshot)) {
+      val replays = (1 to 5) map (math.pow(2, _).toInt) map { latency =>
+        val sample = new File(resDir, s"$target-$latency-$backend.sample")
+        Future(latency -> replay(target, "vcs", Some(sample)))
+      }
+      Await.result(Future.sequence(replays), Duration.Inf) foreach { case (latency, exitcode) =>
+        it should s"replay latency: $latency in vcs" in { assert(exitcode == 0) }
+      }
+    }
   }
-  Await.result(Future.sequence(results), Duration.Inf) foreach { case (latency, exitcode) =>
-    it should s"pass latency $latency" in { assert(exitcode == 0) }
-  }
+  compileReplay(new PointerChaser(seed)(tp), "vcs")
+  runTests("verilator")
+  runTests("vcs")
+  println(s"[SEED] ${seed}")
 }
-
-class PointerChaserCppTests extends PointerChaserTestSuite("verilator")
-class PointerChaserVCSTests extends PointerChaserTestSuite("vcs")
+class PointerChaserTests extends PointerChaserTestSuite()

3daf89986cc028e4b120e76a2338e8065dc86784
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-10-18 22:23:09 -0700

    painful build system refactoring

--- project/build.scala
+++ project/build.scala
@@ -2,27 +2,26 @@ import sbt._
 import Keys._
 
 object StroberBuild extends Build {
-  lazy val testlib = settingKey[Unit]("compile test libraries")
+  val chiselVersion = "3.0-BETA-SNAPSHOT"
+  val firrtlVersion = "0.2-BETA-SNAPSHOT"
   override lazy val settings = super.settings ++ Seq(
     scalaVersion := "2.11.7",
-    scalacOptions ++= Seq("-deprecation","-unchecked"),
-    testlib := {
-      val dir = s"${baseDirectory.value}/strober/src/main/cc"
-      val make = Seq("make", "-C", dir)
-      assert((make :+ s"$dir/utils/libemul.a").! == 0)
-      assert((make :+ s"$dir/utils/libreplay.a").! == 0)
-    }
+    libraryDependencies += "edu.berkeley.cs" %% "chisel3" % chiselVersion,
+    resolvers ++= Seq(Resolver.sonatypeRepo("snapshots"), Resolver.sonatypeRepo("releases"))
   )
-  lazy val chisel    = project in file("riscv-mini/chisel")
-  lazy val firrtl    = project in file("riscv-mini/firrtl")
-  lazy val interp    = project in file("riscv-mini/interp") dependsOn firrtl
-  lazy val testers   = project in file("riscv-mini/testers") dependsOn (chisel, interp)
-  lazy val tutorial  = project dependsOn testers
-  lazy val cde       = project in file("riscv-mini/cde") dependsOn chisel
-  lazy val junctions = project in file("riscv-mini/junctions") dependsOn cde
-  lazy val mini      = project in file("riscv-mini") dependsOn (junctions, testers)
-  lazy val strober   = project dependsOn (firrtl, junctions)
+  lazy val testlib = settingKey[Unit]("compile test libraries")
+  lazy val compileTestLib = Seq(testlib := {
+    val dir = s"${baseDirectory.value}/src/main/cc"
+    val make = Seq("make", "-C", dir)
+    assert((make :+ s"$dir/utils/libemul.a").! == 0)
+    assert((make :+ s"$dir/utils/libreplay.a").! == 0)
+  })
+  lazy val firrtl    = project
+  lazy val tutorial  = project
+  lazy val cde       = project in file("riscv-mini/cde") dependsOn firrtl
+  lazy val junctions = project in file("riscv-mini/junctions") dependsOn cde settings (settings:_*) 
+  lazy val mini      = project in file("riscv-mini") dependsOn junctions
+  lazy val strober   = project dependsOn junctions
   lazy val memModel  = project in file("midas-memory-model") dependsOn strober
-  lazy val root      = project in file(".") settings (settings:_*) dependsOn (
-    tutorial, mini % "compile->compile;test->test", memModel)
+  lazy val root      = project in file(".") dependsOn (tutorial, mini, memModel)
 }

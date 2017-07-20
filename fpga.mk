b7b9771a973f5423698e39e4ab26a4feabbbee6d
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-07-20 11:30:58 -0700

    switch to strober-zynq

--- fpga.mk
+++ fpga.mk
@@ -45,7 +45,7 @@ $(PLATFORM): $(out_dir)/$(DESIGN)-$(PLATFORM) $(out_dir)/$(DESIGN).chain
 ifeq ($(PLATFORM),zynq)
 # Generate bitstream
 board     ?= zedboard
-board_dir := $(base_dir)/midas-$(PLATFORM)/$(board)
+board_dir := $(base_dir)/strober-$(PLATFORM)/$(board)
 bitstream := fpga-images-$(board)/boot.bin
 
 $(board_dir)/src/verilog/$(DESIGN)/$(shim).v: $(verilog)
